a47650b48ea498195dcb6896470e00647d782f07
Tomas Turek <tturek@redhat.com>
2020-01-23 13:55:25 +0100

    Use named query

--- karaf4/camel-jpa/src/main/java/com/github/osmman/karaf4/entity/Book.java
+++ karaf4/camel-jpa/src/main/java/com/github/osmman/karaf4/entity/Book.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedNativeQuery;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.TableGenerator;
